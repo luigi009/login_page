68bdf36f150c977932c7b68019a7777f6012e927
Luigi Fanaro <luigifanaro@hotmail.com>
2023-01-29 15:21:53 -0300

    Alterado a mensagem de erro do modal

--- src/components/Feedback/const.ts
+++ src/components/Feedback/const.ts
@@ -10,7 +10,8 @@ export const feedbackTypes = {
     iconSrc: "/svg/error.svg",
     iconAlt: "Ícone de erro",
     title: "Error",
-    paragraph: "You need to fill in all fields",
+    paragraph:
+      "You need to fill in all fields. Enter your email and password with at least 8 characters",
     buttonText: "Try again",
   },
   warning: {

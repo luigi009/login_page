656b135efbdcd218e35f599ca2a67d0a69c01324
Luigi Fanaro <luigifanaro@hotmail.com>
2023-01-29 18:11:57 -0300

    Bloqueado o botão de login quando clica na primeira vez

--- src/components/SocialMediaButton/index.tsx
+++ src/components/SocialMediaButton/index.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState, useEffect, use } from "react";
 import { signIn, useSession } from "next-auth/react";
 import Button from "../Button";
 import Image from "next/image";
 
 function SocialMediaButton() {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
+
+  const onclickGitHub = () => {
+    setLoading(true);
+    signIn("github", {
+      callbackUrl: `/User/${session && session.user.name}`,
+    });
+  };
+
+  useEffect(() => {
+    if (session) {
+      setLoading(false);
+    }
+  }, [session]);
+
   return (
     <>
       <Button
+        disabled={loading}
         type="button"
-        onClick={() =>
-          signIn("github", {
-            callbackUrl: `/User/${session && session.user.name}`,
-          })
-        }
+        onClick={onclickGitHub}
         className={
           "flex flex-row justify-start items-center pointer w-full my-4 text-xl bg-[#444444] text-white border-2 border-[#444444] p-2 rounded-full uppercase font-bold hover:opacity-75 ease-in-out duration-300"
         }

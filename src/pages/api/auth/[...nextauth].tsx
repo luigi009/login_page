57fe03dc8bece350ea147a1c981d4b1208451bc2
Luigi Fanaro <luigifanaro@hotmail.com>
2023-01-30 09:19:41 -0300

    Adicionado tipagem no client id

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -4,8 +4,8 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     CredentialsProvider({
       type: "credentials",
